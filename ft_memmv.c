050665fe51ea9c70d9948c75a9fbc57331776fba
NFAbricio <fabnunes014@gmail.com>
2026-06-02 11:25:17 -0300

    mem and strl

---
+++ ft_memmv.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmv.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/02 10:48:18 by marvin            #+#    #+#             */
+/*   Updated: 2026/06/02 10:48:18 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+    int i;
+
+    if (!dst || !src)
+        return (NULL);
+    if (dst >= src)
+    {
+        i = (int)len - 1;
+        while (i >= 0)
+        {
+            *(char*)(dst + i) = *(char*)(src + i);
+            i--;
+        }
+    }
+    else
+    {
+        i = 0;
+        while (i < (int)len)
+        {
+            *(char*)(dst + i) = *(char*)(src + i);
+            i++;
+        }
+    }
+    return (dst);
+}
