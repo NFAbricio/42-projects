0e9e86a15adf8bb55e43b2303c39b96e807dea44
NFAbricio <fabnunes014@gmail.com>
2026-06-22 12:37:34 -0300

    libft

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 10:42:59 by marvin            #+#    #+#             */
+/*   Updated: 2026/06/22 10:42:59 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list  *new;
+    if (!lst || !f)
+        return (NULL);
+    new = ft_lstnew(f(lst->content));
+    if (!new)
+    {
+        ft_lstclear(&new, del);
+        return (NULL);
+    }
+    new->next = ft_lstmap(lst->next, f, del);
+    return (new);
+}
